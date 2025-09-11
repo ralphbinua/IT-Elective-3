2e3d4449a3a47718d550430a135e26f452352b45
Ralph Binua <binua.ralphgabrielb@gmail.com>
2025-09-11 13:55:18 +0800

    new

--- Lab_Act2/app.js
+++ Lab_Act2/app.js
@@ -1,3 +1,28 @@
 // Binua, Ralph Gabriel B.
 //3BSIT-5
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const book = [
+   {
+     id: 1,
+     title: "Application Development",
+     author: "Ralph Binua"
+   },
+   {
+     id: 2,
+     title: "Information Management",
+     author: "Juan Dela Cruz"
+   },
+   {
+     id: 3,
+     title: "Free Elective",
+     author: "Jose Rizal"
+   }
+];
+
+
